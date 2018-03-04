af8ec5c0a3223d7ebe13a2366bddedc62b4b050e
kehindeowoeye <kehindeowoeye@yahoo.com>
2018-03-04 21:31:28 +0000

    Update mutual_info.m

--- mutual_info.m
+++ mutual_info.m
@@ -4,6 +4,7 @@ function gr = mutual_info(dez)
 %https://uk.mathworks.com/matlabcentral/fileexchange/29039-mutual-information-2-variablle?focused=5169316&tab=function
 %The input should be a columnn wise matrix of the
 %time-series of interest. 
+dez = xlsread('data');
 gh = [];gr = []; 
 for j = 1:size(dez,2)
     for k = 1:size(dez,2)
